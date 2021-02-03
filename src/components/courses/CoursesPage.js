b87d1160bba271201b31d29927b8a8ef8ca74121
NahSama <hoangnguyendinh1107@gmail.com>
2021-02-03 11:43:40 +0800

    Async in Redux

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,39 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as courseActions from '../../redux/actions/courseActions';
+import * as authorActions from '../../redux/actions/authorActions';
 import PropTypes from 'prop-types';
 import {bindActionCreators} from 'redux';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
-    
-    state = {
-        course: {
-            title: ""
+
+    componentDidMount(){
+        const { courses, actions, authors } = this.props;
+         
+        if(courses.length === 0 ){
+            actions.loadCourses().catch(err => {
+                alert("Loading Courses fail" + err);
+            });
         }
-    }
-
-    handleChange = (event) => {
-        const course = {...this.state.course, title: event.target.value};
-        this.setState({course: course});
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        // debugger;
-        this.props.actions.createCourse(this.state.course);
+        
+        if(authors.length === 0){
+            actions.loadAuthors().catch(err => {
+                alert("Loading Authors fail" + err);
+            })
+        }
     }
 
     render() {
         return (
-            <form onSubmit={this.handleSubmit} >
-                <h2>Courses</h2>
-                <h3>Add Course</h3>
-                <input type="text" onChange={this.handleChange} value={this.state.course.title}></input>
-                <input type="submit" value="Save"/>
-                {this.props.courses.map(course => (
-                    <div key={course.title}>{course.title}</div>
-                ))}
-            </form>
+            <>
+            <h2>Courses</h2>
+            <CourseList courses={this.props.courses}/>
+            </>
         )
     }
 }
@@ -42,19 +38,31 @@ class CoursesPage extends Component {
 
 CoursesPage.propTypes = {
     courses: PropTypes.array.isRequired,
-    actions: PropTypes.object.isRequired
-}
+    actions: PropTypes.object.isRequired,
+    authors: PropTypes.array.isRequired
+}   
 
 function mapStateToProps(state){
     // debugger;
     return {
-        courses: state.courses
+        courses: state.authors.length === 0 
+            ? [] 
+            : state.courses.map(course => {
+                return {
+                    ...course,
+                    authorName: state.authors.find(a => a.id === course.authorId).name
+                }   
+            }),
+        authors: state.authors
     }
 }
 
 function mapDispatchToProps(dispatch){
     return {
-        actions: bindActionCreators(courseActions, dispatch)
+        actions: {
+            loadCourses: bindActionCreators(courseActions.loadCourses, dispatch),
+            loadAuthors: bindActionCreators(authorActions.loadAuthors, dispatch)
+        }
     }
 }
 
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxImmutatbleStateInvariant from 'redux-immutable-state-invariant';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
     const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for Redux dev tooll
     return createStore(
         rootReducer, 
         initialState, 
-        composeEnhancer(applyMiddleware(reduxImmutatbleStateInvariant()))
+        composeEnhancer(applyMiddleware(thunk, reduxImmutatbleStateInvariant()))
     );
 }
