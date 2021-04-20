fea4cdb985a0b0eb4804917479f174bc7091ee3f
NahSama <hoangnguyendinh1107@gmail.com>
2021-04-20 23:33:48 +0800

    Async status handle

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -5,6 +5,7 @@ import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
 import CourseForm from "./CourseForm";
 import { newCourse } from "../../../tools/mockData";
+import Spinner from "../common/Spinner";
 
 function ManageCoursePage({
   courses,
@@ -17,6 +18,7 @@ function ManageCoursePage({
 }) {
   const [course, setCourse] = useState({ ...props.course });
   const [errors, setErrors] = useState({});
+  const [saving, setSaving] = useState(false);
 
   useEffect(() => {
     if (courses.length === 0) {
@@ -43,18 +45,40 @@ function ManageCoursePage({
 
   function handleSave(event) {
     event.preventDefault();
+    if (!formIsValid()) {
+      console.log(errors);
+      return;
+    }
+    setSaving(true);
     saveCourse(course).then(() => {
       history.push("/courses");
+    }).catch(error => {
+      setSaving(false);
+      setErrors({onSave : error.message});
     });
   }
 
+  function formIsValid(){
+    const {title, authorId, category} = course;
+    const errors = {};
+
+    if (!title) errors.title = "Title is required";
+    if (!authorId) errors.author = "Author is required";
+    if (!category) errors.category = "Category is required";
+    setErrors(errors);
+    return Object.keys(errors).length === 0;
+  }
+
   return (
-    <CourseForm
+    courses.length === 0 || authors.length === 0 
+    ? <Spinner/> 
+    :<CourseForm
       course={course}
       errors={errors}
       authors={authors}
       onChange={handleChange}
       onSave={handleSave}
+      saving={saving}
     />
   );
 }
