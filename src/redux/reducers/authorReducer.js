d18e552f2eebbfd56ee0c4f72aa7702114079d11
NahSama <hoangnguyendinh1107@gmail.com>
2021-04-20 17:10:33 +0800

    configure CoursesPage and add CourseList

--- src/redux/reducers/authorReducer.js
+++ src/redux/reducers/authorReducer.js
@@ -2,13 +2,11 @@ import * as types from "../actionTypes";
 import initialState from "../initialState";
 
 // eslint-disable-next-line no-unused-vars
-export default function courseReducer(state = initialState.courses, action){
+export default function authorReducer(state = initialState.authors, action){
     switch (action.type){
-        case types.CREATE_COURSE: 
-            return [...state, {...action.course}];
         //The new state is fetched from the api, no need to hold the prevState
-        case types.LOAD_COURSES_SUCCESS:
-            return action.courses;
+        case types.LOAD_AUTHORS_SUCCESS:
+            return action.authors;
         default: 
             return state;
     }
